0ffa8665103abe2b3f8e54bf16623cb0b467b87d
Rapin <rapin.pakdee@gmail.com>
2024-03-27 16:21:24 +0700

    add GetUsers and GetUserFromID to interface of user_store

--- db/users_store.go
+++ db/users_store.go
@@ -3,14 +3,18 @@ package db
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/rapinbook/hotel-reservation-go/types"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type UserStore interface {
 	InsertUser(context.Context, *types.User) (primitive.ObjectID, error)
+	GetUserByID(context.Context, primitive.ObjectID) (*types.User, error)
+	GetUsers(context.Context) ([]*types.User, error)
 }
 
 type MongoUserStore struct {
@@ -34,4 +38,44 @@ func (s *MongoUserStore) InsertUser(c context.Context, user *types.User) (primit
 	}
 	id := res.InsertedID.(primitive.ObjectID)
 	return id, nil
+}
+
+// func (s *MongoUserStore) GetUserByID(c context.Context, ID primitive.ObjectID) (*types.User, error) {
+// 	return _, nil
+// }
+
+func (s *MongoUserStore) GetUsers(c context.Context) ([]*types.User, error) {
+	var allUsers []*types.User
+	cur, err := s.coll.Find(c, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cur.Close(c)
+	for cur.Next(c) {
+		var result *types.User
+		err := cur.Decode(&result)
+		if err != nil {
+			log.Fatal(err)
+		}
+		allUsers = append(allUsers, result)
+		// do something with result....
+	}
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return allUsers, nil
+}
+
+func (s *MongoUserStore) GetUserByID(c context.Context, objID primitive.ObjectID) (*types.User, error) {
+	var user *types.User
+	filter := bson.M{"_id": objID}
+	err := s.coll.FindOne(c, filter).Decode(&user)
+	if err == mongo.ErrNoDocuments {
+		// Do something when no record was found
+		fmt.Println("record does not exist")
+	} else if err != nil {
+		log.Fatal(err)
+	}
+	return user, nil
 }
--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
 	"time"
 
 	"github.com/rapinbook/hotel-reservation-go/db"
-	"github.com/rapinbook/hotel-reservation-go/types"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -24,14 +26,58 @@ func main() {
 		log.Fatal(err)
 	}
 	mongoClient := db.NewMongoUserStore(client)
-	_, err = mongoClient.InsertUser(ctx, &types.User{
-		Email:     "foo@foobar.com",
-		FirstName: "Josh",
-		LastName:  "Gunn",
-	})
+	// _, err = mongoClient.InsertUser(ctx, &types.User{
+	// 	Email:     "John@foobar.com",
+	// 	FirstName: "John",
+	// 	LastName:  "Gunn",
+	// })
+	// if err != nil {
+	// 	log.Fatalf("Cannot inserted row to databases")
+	// }
+	// _, err = mongoClient.InsertUser(ctx, &types.User{
+	// 	Email:     "James@foobar.com",
+	// 	FirstName: "James",
+	// 	LastName:  "Gunn",
+	// })
+	// if err != nil {
+	// 	log.Fatalf("Cannot inserted row to databases")
+	// }
+	// _, err = mongoClient.InsertUser(ctx, &types.User{
+	// 	Email:     "Josh@foobar.com",
+	// 	FirstName: "Josh",
+	// 	LastName:  "Gunn",
+	// })
+	// if err != nil {
+	// 	log.Fatalf("Cannot inserted row to databases")
+	// }
+	allUsers, err := mongoClient.GetUsers(ctx)
 	if err != nil {
-		log.Fatalf("Cannot inserted row to databases")
+		fmt.Println(err)
 	}
-	// fmt.Printf()("Inserted row first run %v", insertedRow.Hex())
-
+	// _, err := json.Marshal(allUsers)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	for _, s := range allUsers {
+		b, err := json.Marshal(s)
+		fmt.Println(string(b))
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	fmt.Println("Above show all rows in system")
+	objID, err := primitive.ObjectIDFromHex("6603dfdcff4c801f4a9e6599")
+	if err != nil {
+		panic(err)
+	}
+	user, err := mongoClient.GetUserByID(ctx, objID)
+	if err != nil {
+		fmt.Println(err)
+	}
+	b, err := json.Marshal(user)
+	fmt.Println(string(b))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Above show row filter from obj")
 }
